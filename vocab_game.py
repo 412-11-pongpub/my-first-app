94f838c732b6120053d7cb8954b5cdac27c6f098
412-11-pongpub <54289@yupparaj.ac.th>
2026-09-01 10:29:00 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -101,4 +101,4 @@ if st.session_state.get("is_ended", False):
     show_result_dialog(ans1, ans2)
 
 st.divider()
-  st.write("นายปองภพ สิงห์แก้ว เลขที่ 11 ม.4/11")
+  st.write("นายปองภพ สิงห์แก้ว เลขที11 ม.4/11")
